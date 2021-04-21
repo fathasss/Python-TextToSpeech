a39c538c994d0922b5e1340f132d102feee5f636
Fatih HAS <32196738+fathasss@users.noreply.github.com>
2021-04-21 17:01:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,60 @@
 # %%
+
+#Google Kütüphaneleri
 from gtts import gTTS
 from google_trans_new import google_translator
 import os
+
 # %%
+
+#Metin girişi
 text = input("Metin giriniz.... ")
 
+#Çeviri aracı
 translator = google_translator()
 
+#Girilen türkçe metnin çevirisni yap.
 textTranslaterENG = translator.translate(text, lang_src='tr' ,lang_tgt='en')
 textTranslaterFR = translator.translate(text, lang_src='tr' ,lang_tgt='fr')
 textTranslaterGER = translator.translate(text, lang_src='tr' ,lang_tgt='de')
 
+#Metni ses dönüştür.
 speech_tr= gTTS(text=text, lang='tr', slow=False)
 speech_en= gTTS(text=textTranslaterENG, lang='en', slow=False)
 speech_de= gTTS(text=textTranslaterGER, lang='de', slow=False)
 speech_fr= gTTS(text=textTranslaterFR, lang='fr', slow=False)
 
-speech_tr.save("tr.mp3")
-speech_en.save("en.mp3")
-speech_de.save("de.mp3")
-speech_fr.save("fr.mp3")
+#Ses dosyalarını kaydet
+speech_tr.save("mp3/tr.mp3")
+speech_en.save("mp3/en.mp3")
+speech_de.save("mp3/de.mp3")
+speech_fr.save("mp3/fr.mp3")
 
+#Desteklenen dilleri konsola yaz.
 os.system("echo Listeden dil seciniz.")
 os.system("echo 1.Turkish")
 os.system("echo 2.English")
 os.system("echo 3.French")
 os.system("echo 4.German")
 
+#Dil seçimi için veri al.
 out = int(input("Secmek istediginiz dilin numarasi nedir ? ..... "))
 
+#Girilen out değerine göre çalıştır.
 if out==1:
-    os.system("start tr.mp3")
+    print("Türkçe")
+    print(text)
+    os.system("start mp3/tr.mp3")
 if out==2:
-    os.system("start en.mp3")
+    print("İngilizce")
+    print(textTranslaterENG)
+    os.system("start mp3/en.mp3")
 if out==3:
-    os.system("start fr.mp3")
+    print("Fransızca")
+    print(textTranslaterFR)
+    os.system("start mp3/fr.mp3")
 if out==4:
-    os.system("start de.mp3")    
+    print("Almanca")
+    print(textTranslaterGER)
+    os.system("start mp3/de.mp3")
 # %%
